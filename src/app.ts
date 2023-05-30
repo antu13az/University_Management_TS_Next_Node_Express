6a75bff3fd7d104cb171337ea88cabced803ff99
@ ANTU <sajalhowlader08@gmail.com>
2023-05-30 21:10:28 +0600

    add husky

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,21 @@
-import express, { Application,Request,Response } from "express";
-import cors from "cors";
-import dbConnect from "./utils/databaseConnection";
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+import dbConnect from './utils/databaseConnection'
 
-const app:Application = express();
-
+const app: Application = express()
 
 // Middleware
-app.use(cors());
+app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({ extended:false}))
+app.use(express.urlencoded({ extended: false }))
 
 // Database Connection
 
 dbConnect()
 
 // Root Api
-app.get("/", (req:Request, res:Response) => {
-  res.send("Hello World!");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
 
-
-export default app;
+export default app
--- src/utils/databaseConnection.ts
+++ src/utils/databaseConnection.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import config from "../config";
+import mongoose from 'mongoose'
+import config from '../config'
 
 const dbConnect = async () => {
   try {
     if (!config.database_url) {
-      console.log("No Uri Found");
-      process.exit(1);
+      console.log('No Uri Found')
+      process.exit(1)
     }
-    await mongoose.connect(config.database_url as string);
-    console.log("🔥 Database Connection Successful");
+    await mongoose.connect(config.database_url as string)
+    console.log('🔥 Database Connection Successful')
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
-};
+}
 
-export default dbConnect;
+export default dbConnect

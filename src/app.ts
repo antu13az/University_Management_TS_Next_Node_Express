eb7892ac66188b1844ffb29a1bcec26b5bcd94eb
@ ANTU <sajalhowlader08@gmail.com>
2023-05-30 14:01:52 +0600

    Done database express dotenv mongoose

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express, { Application,Request,Response } from "express";
+import cors from "cors";
+import dbConnect from "./utils/databaseConnection";
+
+const app:Application = express();
+
+
+// Middleware
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended:false}))
+
+// Database Connection
+
+dbConnect()
+
+// Root Api
+app.get("/", (req:Request, res:Response) => {
+  res.send("Hello World!");
+});
+
+
+export default app;
---
+++ src/utils/databaseConnection.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import config from "../config";
+
+const dbConnect = async () => {
+  try {
+    if (!config.database_url) {
+      console.log("No Uri Found");
+      process.exit(1);
+    }
+    await mongoose.connect(config.database_url as string);
+    console.log("🔥 Database Connection Successful");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default dbConnect;
